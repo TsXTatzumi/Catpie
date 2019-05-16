d10259153aeb14cbe399f54dbaf2241688727824
TsXTatzumi <31726337+TsXTatzumi@users.noreply.github.com>
2019-05-16 22:34:56 +0200

    final bac

---
+++ examples/Python/R/speech recognition stats_V2.R
@@ -0,0 +1,33 @@
+##
+## BAC Stats 
+## Simeon Quant
+## 19.01.2019
+##
+## V2: data from files
+
+library(gmodels)
+library(MASS)
+
+##
+## enter data in the following order
+##
+## condition 1 <# of state 1> <# of state 2>
+## condition 2 <# of state 1> <# of state 2>
+##
+## Example with the data below
+##       l_silence l_noise
+## RIGHT        87      70
+## WRONG        13      30
+##
+
+
+## data from the accuracy test in silence and noise
+## Chisquare analysis of the 3 data sets from silence and noise
+word <- as.matrix(read.table(file = "blinke_links.data"))
+CrossTable(word, fisher = TRUE, chisq = TRUE, expected = TRUE, sresid = TRUE, format = "SPSS")
+
+word <- as.matrix(read.table(file = "blinke_rechts.data"))
+CrossTable(word, fisher = TRUE, chisq = TRUE, expected = TRUE, sresid = TRUE, format = "SPSS")
+
+word <- as.matrix(read.table(file = "schalte_runter.data"))
+CrossTable(schalte_runter, fisher = TRUE, chisq = TRUE, expected = TRUE, sresid = TRUE, format = "SPSS")
